1ac71b9ce6ce51a59cfbfab698cbb5d6925ae8ec
sbx_user1051 <sbx_user1051@169.254.109.141>
2024-05-30 11:22:50 +0000

    Thu May 30 11:22:50 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -12,17 +12,22 @@ def checkout(skus):
             sum += prices.get(item)
         else:
             return -1  
+    print(sum)
     if skus.count("A") > 2:
         for n in range(1, skus.count("A")):
             if n % 3 == 0:
                 a_special += 20
+                print("ln19 a discount applied")
         sum = sum - a_special
     if skus.count("B") > 1:
         for m in range(1, skus.count("B")):
             if m % 2 == 0:
                 b_special += 15
         sum = sum - b_special
+    
     return sum
+
+checkout("AAAA")
 
 
 
