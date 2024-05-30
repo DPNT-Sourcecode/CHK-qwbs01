ed168f588b88b9ff8e4aa6542478e539c7059ae6
sbx_user1051 <sbx_user1051@169.254.109.141>
2024-05-30 13:15:40 +0000

    Thu May 30 13:15:40 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,27 +3,30 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    total = 0
-    prices = {"A":50, "B": 30, "C": 20, "D":15, "E": 40}  
-    a_count = skus.count("A")
-
     if len(skus) == 0:
         return -1
     
+    total = 0
+    prices = {"A":50, "B": 30, "C": 20, "D":15, "E": 40}  
+    
+    a_count = skus.count("A")
     while a_count > 0:
         if a_count % 5 == 0:
             total -= 50
             a_count -= 5
         elif a_count % 3 == 0:
             total -= 20
+    
     if skus.count("B") > 1:
         for m in range(1, skus.count("B") + 1):
             if m % 2 == 0:
                 total -= 15
+    
     if skus.count("E") > 1:
         for m in range(1, skus.count("B") + 1):
             if m % 2 == 0:
                 total -= 40
+    
     for item in skus:
         if item in prices:
             total += prices.get(item)
@@ -38,5 +41,6 @@ checkout("AAAAAAAAAAAAAAA")
 # checkout("AAABBBBBBBCCCDDDE")
 # checkout("F")
 # checkout("a")
+
 
 
