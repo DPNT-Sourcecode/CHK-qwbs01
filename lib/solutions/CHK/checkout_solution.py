6bb41ef47410d8cba532ec59ddfa9622a39a3417
sbx_user1051 <sbx_user1051@169.254.109.141>
2024-05-30 11:02:24 +0000

    Thu May 30 11:02:24 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,15 +3,18 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    # dict with values of each sku
+    sum = 0
     prices = {"A":50, "B": 30, "C": 20, "D":15}
     for item in skus:
-        
-    # for letter in sku, find value, add to sum
+        if 
+            return -1
+        else:
+            sum += prices.get(item)
     # discounts... if A > 3... find number of times 3 goes into As, discount as necessary
         # use modulo?  
-    sum = 0
+    
     return sum
+
 
 
 
