b0ba79028cabc48e57dc79f7948b5f6c7e7594d6
sbx_user1051 <sbx_user1051@169.254.109.141>
2024-05-30 11:54:49 +0000

    Thu May 30 11:54:49 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,22 +5,14 @@
 def checkout(skus):
     sum = 0
     prices = {"A":50, "B": 30, "C": 20, "D":15, "E": 40}  
-    # if skus.count("A") > 2:
-    #     for n in range(1, skus.count("A") + 1):
-    #         if n % 15 == 0:
-    #             sum -= 50
-    #         elif n% 5 == 0:
-    #             sum -= 50
-    #         elif n % 3 == 0:
-    #             sum -= 20
     a_count = 0
     a_count = skus.count("A")
     while a_count > 0:
-        if a_count >= 5:
+        if a_count % 5 == 0:
             sum -= 50
             a_count -= 5
-        
-
+        if a_count % 3 == 0:
+            sum -= 20
     if skus.count("B") > 1:
         for m in range(1, skus.count("B") + 1):
             if m % 2 == 0:
@@ -35,5 +27,3 @@ def checkout(skus):
         else:
             return -1
     return sum
-
-
