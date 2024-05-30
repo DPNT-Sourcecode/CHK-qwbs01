29c54c62808b80dafef3dbb9582a4fa70ae8ac72
sbx_user1051 <sbx_user1051@169.254.109.141>
2024-05-30 11:05:24 +0000

    Thu May 30 11:05:24 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,15 +6,11 @@ def checkout(skus):
     sum = 0
     prices = {"A":50, "B": 30, "C": 20, "D":15}
     for item in skus:
-        if 
-            return -1
-        else:
+        if item in prices:
             sum += prices.get(item)
-    # discounts... if A > 3... find number of times 3 goes into As, discount as necessary
-        # use modulo?  
-    
+        else:
+            return -1  
+    if skus.count("A") > 2:
+        #calculate discount
+        pass
     return sum
-
-
-
-
