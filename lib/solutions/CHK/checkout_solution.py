dada55eded4ef2a5b640c4b1bb80024a441a0b0b
sbx_user1051 <sbx_user1051@169.254.109.141>
2024-05-30 10:56:24 +0000

    Thu May 30 10:56:24 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,7 +3,10 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    
+    # dict with values of each sku
+    # for letter in sku, find value, add to sum
+    # discounts... if A > 3... find number of times 3 goes into As, discount as necessary
     sum = 0
     return sum
+
 
