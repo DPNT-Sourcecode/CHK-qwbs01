bc97c109517b118aa85b761eaf3badcb80b3f85e
sbx_user1051 <sbx_user1051@169.254.109.141>
2024-05-30 11:34:55 +0000

    Thu May 30 11:34:55 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,13 +4,13 @@
 # skus = unicode string
 def checkout(skus):
     sum = 0
-    a_special = 0
-    b_special = 0
     prices = {"A":50, "B": 30, "C": 20, "D":15}  
     if skus.count("A") > 2:
         for n in range(1, skus.count("A") + 1):
+            print(n)
             if n % 3 == 0:
-                sum =- 20
+                sum -= 20
+                print(sum)
     if skus.count("B") > 1:
         for m in range(1, skus.count("B") + 1):
             if m % 2 == 0:
@@ -20,9 +20,6 @@ def checkout(skus):
             sum += prices.get(item)
         else:
             return -1
-    print(sum - a_special - b_special)
-    return sum - a_special - b_special
+    return sum
 
-checkout("AAA")
 checkout("AAAAAA")
-checkout("BB")
